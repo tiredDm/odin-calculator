66b14139a425a08ebe5d25923c65906d371f348e
tiredDm <ronpri1@gmail.com>
2023-09-13 10:16:39 -0400

    Made operate

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+let firstNumber;
+let operator;
+let secondNumber;
+
 function add (a,b){
     return a+b;
 }
@@ -12,4 +17,16 @@ function multiply (a,b){
 
 function divide (a,b){
     return a/b;
+}
+
+function operate(fN, op, sN){
+    if(op == '+'){
+        return add(fN, sN);
+    } else if(op == '-'){
+        return subtract(fN,sN);
+    } else if(op == '*') {
+        return multiply(fN,sN);
+    } else {
+        return divide(fN,sN);
+    }
 }
