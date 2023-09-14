892534fdb2b0c6d6fc588e5f66d527c928285dd1
tiredDm <ronpri1@gmail.com>
2023-09-14 08:07:47 -0400

    ADD all other event listeners and appropriate javascript to perform calculator tasks

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
 
-let firstNumber;
-let operator;
-let secondNumber;
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
 let displayValue = document.querySelector('.display');
 let buttons = document.querySelector('.buttons');
 let numberButtons = Array.from(document.querySelectorAll('.number'));
-
+let operatorButtons = Array.from(document.querySelectorAll('.operator'));
+let clearButton = document.querySelector('.clear');
+let equalsButton = document.querySelector('.equal');
 
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(displayValue.textContent + button.textContent);
+        //console.log(displayValue.textContent + button.textContent);
         if(displayValue.textContent == 0){ //or operator is pressed.
             displayValue.textContent = button.textContent;
         } else {
             displayValue.textContent =  displayValue.textContent + button.textContent;
-        }
-
-        
+        }  
     });
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        
+        if(firstNumber != null){
+            secondNumber = displayValue.textContent;
+            displayValue.textContent = operate(Number(firstNumber), operator,Number(secondNumber));
+            operator = button.textContent;
+            secondNumber = null;
+            firstNumber = null;
+        }
+        
+        if(firstNumber == null){
+            firstNumber = displayValue.textContent;
+            displayValue.textContent = 0;
+            operator = button.textContent;
+            console.log(firstNumber);
+        } 
+
+
+    });
+});
+
+clearButton.addEventListener('click',() => {
+    displayValue.textContent = 0;
+});
+
+equalsButton.addEventListener('click',() => {
+    if(operator) {
+        secondNumber = displayValue.textContent;
+        displayValue.textContent = operate(Number(firstNumber), operator,Number(secondNumber));
+        secondNumber = null;
+        firstNumber = null;
+        operator = null;
+    }
+            
 });
 
 function add (a,b){
@@ -41,7 +78,7 @@ function operate(fN, op, sN){
         return add(fN, sN);
     } else if(op == '-'){
         return subtract(fN,sN);
-    } else if(op == '*') {
+    } else if(op == 'x') {
         return multiply(fN,sN);
     } else {
         return divide(fN,sN);
